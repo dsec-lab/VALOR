2ef0f5a6aea4a277915a48188ae6b89fa215b46e
LR-Security <86996308+dsec-lab@users.noreply.github.com>
2026-09-04 21:38:44 +0800

    Delete AKS directory

--- AKS/test.py
+++
@@ -1,7 +0,0 @@
-# import pandas as pd
-import json
-path = 'outscores/nextqa/blip/video.json'
-with open(path) as file:
-    for line in file.readlines():
-        data = json.loads(line)
-        print(len(set(data)))
